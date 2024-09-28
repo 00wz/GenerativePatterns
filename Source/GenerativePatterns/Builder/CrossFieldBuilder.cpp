2cfd38f09330d0c509ccc229d8a0e21f84bdba11
Pavel <pedaracha@gmail.com>
2024-09-28 20:15:57 +0300

    fix cross builder

--- Source/GenerativePatterns/Builder/CrossFieldBuilder.cpp
+++ Source/GenerativePatterns/Builder/CrossFieldBuilder.cpp
@@ -13,12 +13,13 @@ void UCrossFieldBuilder::BuildBlocks_Implementation()
 	for(int i = 0; i < field->FIELD_SIDE_LENGTH; i++)
 	{
 		field->Field[i][i] = EFieldPointType::BLOCK;
+		field->Field[field->FIELD_SIDE_LENGTH - 1 -i][i] = EFieldPointType::BLOCK;
 	}
 }
 
 void UCrossFieldBuilder::BuildBonuses_Implementation()
 {
-	for (int i = 0; i < field->FIELD_SIDE_LENGTH; i++)
+	for (int i = 1; i < field->FIELD_SIDE_LENGTH - 1; i++)
 	{
 		field->Field[0][i] = EFieldPointType::BONUS;
 	}
